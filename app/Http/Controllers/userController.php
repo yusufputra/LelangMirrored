31ef029a6a0c28660320b5cfbf25ceb96a017e24
Yusuf Putra <yusufputra@Yusufs-iMac.local>
2019-04-20 17:43:48 +0700

    login bener

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -49,7 +49,6 @@ class UserController extends Controller
             ]);
             $data = Pengguna::where('username', $validatedData['username'])
                     ->get();
-            return $data->toJson();
             if($data){
                 if(Hash::check($validatedData['password'], $data[0]->password)){
                     session(['username' => $data[0]->username]);
