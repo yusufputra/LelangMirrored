1f5f00fee398b72e3ae7cd5c3d276fa62db79ec7
Yusuf Putra <yusufputra@Yusufs-iMac.local>
2019-04-23 22:47:17 +0700

    middleware checktoken

---
+++ app/Http/Middleware/CheckJwt.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Firebase\JWT\JWT;
+
+
+class CheckJwt
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        try{
+            $user = JWT::decode($request->header('authorization'),env('SECRET_TOKEN_KEY'),['HS256']);
+        }catch(\Exception $e){
+            return response()->json("Unauthorized",401);
+        }
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,17 +13,23 @@ use Illuminate\Http\Request;
 |
 */
 
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('login', 'UserController@login');
-Route::post('daftar', 'UserController@daftar');
-Route::get('pengguna', 'UserController@index');
-Route::get('pengguna/{username}', 'UserController@getOneUser');
-Route::post('updateProfile', 'UserController@updateProfile');
+Route::post('/login', 'UserController@login');
+Route::post('/daftar', 'UserController@daftar');
+Route::get('/pengguna', 'UserController@index');
+Route::get('/pengguna/{username}', 'UserController@getOneUser');
+
 
 // ShopController
 Route::post('/daftarToko', 'ShopController@createShop');
 Route::get('/toko/{id}', 'ShopController@readShop');
-Route::post('/ubahInformasiToko', 'ShopController@updateShop');
-Route::post('/hapusToko', 'ShopController@deleteShop');
+
+// need to authorized
+Route::group(['middleware'=>['checkjwt']],function(){
+    Route::post('/updateProfile', 'UserController@updateProfile');
+    Route::post('/ubahInformasiToko', 'ShopController@updateShop');
+    Route::post('/hapusToko', 'ShopController@deleteShop');
+});
